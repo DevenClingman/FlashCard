815d44baf7b654e40e3f406294acf82da2414aa8
Deven Clingman <dclingy@gmail.com>
2018-01-16 12:09:20 -0700

    installed and completed initial setup of devise gem for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :cards
   resources :decks
   resources :categories
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'categories#index'
+
 end
