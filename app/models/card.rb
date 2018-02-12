3c2bf7adc596b49329d50c3177bc1e153768a14d
Deven Clingman <dclingy@gmail.com>
2018-02-12 11:36:11 -0700

    updated show page for card and added jquery to allow for toggling of question and answer

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,12 @@ class Card < ApplicationRecord
   belongs_to :deck
   belongs_to :category
   belongs_to :user
+
+  def self.next
+    Card.next(self.id).first
+  end
+
+  def self.previous
+    Card.previous(self.id).first
+  end
 end
