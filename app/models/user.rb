f05bc64d1f9d70f6eff3afc0fb87b18b7fc456db
Deven Clingman <dclingy@gmail.com>
2018-02-27 16:29:42 -0700

    implemented controller concern to allow for additional whitelisted fields in the user table, upated new registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :categories, dependent: :destroy
   has_many :decks, dependent: :destroy
   has_many :cards, dependent: :destroy
+
+  validates_presence_of :first_name, :last_name
 end
